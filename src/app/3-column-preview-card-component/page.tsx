929a7f1162247a4975579dc0b26c193bdec6fa50
Олег Дидечкин <crm.29431@dz.com>
2023-07-28 16:09:37 +0300

    Image path fix for 3 column preview card

--- src/app/3-column-preview-card-component/page.tsx
+++ src/app/3-column-preview-card-component/page.tsx
@@ -1,29 +1,24 @@
 import Card from "@/components/3cpcc/Card";
 
-const baseImagePath =
-  process.env.NODE_ENV === "production"
-    ? "/frontend-mentor-challenges/3-column-preview-card-component"
-    : "/3-column-preview-card-component";
-
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen my-20 dt:flex-row dt:mx-10 dt:my-0">
       <Card
-        image={`${baseImagePath}/icon-sedans.svg`}
+        image="3-column-preview-card-component/icon-sedans.svg"
         name="sedans"
         text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city on on your next road trip."
         textColor="text-3cpcc-bright-orange"
         className="rounded-t-lg bg-3cpcc-bright-orange dt:rounded-l-lg dt:rounded-tr-none"
       />
       <Card
-        image={`${baseImagePath}/icon-suvs.svg`}
+        image="3-column-preview-card-component/icon-suvs.svg"
         name="suvs"
         text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
         textColor="text-3cpcc-dark-cyan"
         className="bg-3cpcc-dark-cyan"
       />
       <Card
-        image={`${baseImagePath}/icon-luxury.svg`}
+        image="3-column-preview-card-component/icon-luxury.svg"
         name="luxury"
         text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
         textColor="text-3cpcc-very-dark-cyan"
