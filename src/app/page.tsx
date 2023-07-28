696bbf8da69c1d2de3a84b98b55d287b9ea0371c
Олег Дидечкин <crm.29431@dz.com>
2023-07-28 16:21:45 +0300

    Social icons scale on hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Desktop from "@/components/Desktop";
 import Mobile from "@/components/Mobile";
 import Link from "next/link";
+import { IconContext } from "react-icons/lib";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { BsTelegram } from "react-icons/bs";
 
@@ -19,19 +20,25 @@ export default function Home() {
           href="https://www.linkedin.com/in/oleg-didechkin/"
           rel="noopener noreferrer"
           target="_blank">
-          <AiFillLinkedin size={50} />
+          <AiFillLinkedin
+            size={40}
+            className="focus:outline-none hover:scale-90"
+          />
         </Link>
         <Link
           href="https://github.com/DBoFury"
           rel="noopener noreferrer"
           target="_blank">
-          <AiFillGithub size={50} />
+          <AiFillGithub
+            size={40}
+            className="focus:outline-none hover:scale-90"
+          />
         </Link>
         <Link
           href="https://t.me/golovakanta"
           rel="noopener noreferrer"
           target="_blank">
-          <BsTelegram size={45} />
+          <BsTelegram size={35} className="focus:outline-none hover:scale-90" />
         </Link>
       </div>
     </main>
